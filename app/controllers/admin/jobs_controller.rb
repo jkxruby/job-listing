76c6820e9671ec5effa53703c0650b5a60d48255
jkxruby <jkxruby@gmail.com>
2017-08-22 09:52:59 +0800

    modify the is_admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -44,11 +44,11 @@ before_action :require_is_admin?
     redirect_to admin_jobs_path,alert: "admin deleted"
   end
 
-  
+
 
 private
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 
 
