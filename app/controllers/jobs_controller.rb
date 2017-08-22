d0f6e59f8df083393d6f9e6a4ddff9bf45602c93
jkxruby <jkxruby@gmail.com>
2017-08-22 14:56:23 +0800

    add order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,15 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy, :new, :create]
 
 def index
+  @jobs = case params[:order]
+  when 'by_lower_bound'
+    Job.where(is_hidden: false).order('wage_lower_bound DESC')
+  when 'by_upper_bound'
+    Job.where(is_hidden: false).order('wage_upper_bound DESC')
+  else
+
   @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+end 
 end
 
 def show
